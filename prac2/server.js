d096305a58a60f9965d5b0331af76de2273afad0
YashAuti-24 <yashauti298@gmail.com>
2025-07-24 10:19:55 +1000

    Task 2.2P

--- prac2/server.js
+++ prac2/server.js
@@ -1,9 +1,27 @@
 var express = require("express")
+const path = require('path');
 var app = express()
-var port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Hello, Express is running!");
+var port = process.env.port || 3002;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/add', (req, res) => {
+
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
+  
+  const sum = a + b;
+  
+  res.send(`The sum of ${a} and ${b} is: ${sum}`);
 });
-app.listen(port,()=>{
-console.log("App running on port: "+port)
-})
+
+app.get('/square', (req, res) => {
+const num = parseFloat(req.query.num);
+const square = num * num;
+res.send(`The square of ${num} is: ${square}`);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
