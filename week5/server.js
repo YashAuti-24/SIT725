03d416cb465465b7be1b99df6d0bc05f080c0b95
YashAuti-24 <yashauti298@gmail.com>
2025-08-14 12:05:05 +1000

    Week 5 prac

---
+++ week5/server.js
@@ -0,0 +1,37 @@
+var express = require("express")
+var app = express()
+var port = process.env.port || 3004
+const mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+console.log('Connected to MongoDB');
+});
+
+const ProjectSchema = new mongoose.Schema({
+title: String,
+image: String,
+link: String,
+});
+const Project = mongoose.model('Project', ProjectSchema);
+
+const sampleProject = new Project({
+    title: "Burwood campus",
+    image: "deakin1.png",
+    link: "About this campus",
+  });
+
+app.get('/projects', async (req, res) => {
+const projects = await Project.find({});
+res.json({ statusCode: 200, data: projects, message: 'Success' });
+});
+
+app.listen(port, () => {
+console.log(`App listening on port ${port}`);
+});
